ec38c9f9bc51e1b9656842ae304f36ab7b05ec1b
Yasu Flores <carlosyasu91@gmail.com>
2016-11-12 09:30:15 -0800

    .gitignore now works

--- public/js/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export default {
-  ENTER_NAME: 'Please enter a name'
-}
--- public/js/utils/sockets.js
+++
@@ -1,6 +0,0 @@
-const socket = io();
-
-export const socketEvent = (eventName, data) => {
-  socket.emit(eventName, data);
-  console.log('emitted', eventName, 'with Data', data)
-}
