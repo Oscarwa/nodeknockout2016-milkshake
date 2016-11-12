b9a875bc15a0d5efbf6b22fac3a59590551fed8f
Oscar Barba <racso_181088@hotmail.com>
2016-11-12 00:12:50 -0600

    Added bouncing to target

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 // socket.io events and handles
 const socket = io();
-let USERNAME = ''
+let USERNAME = '';
+var target;
 
 const socketEvent = (eventName, data) => {
   socket.emit(eventName, data);
@@ -11,16 +12,47 @@ socket.on('shoot', (data) => {
 });
 
 // Phaser io events and handles
+function createTarget() {
+  var target = game.add.sprite(100, 120, 'target');
+  target.anchor.set(0.5);
+  target.scale.set(0.2);
+  game.physics.arcade.enable(target);
+  target.inputEnabled = true;
+  target.events.onInputDown.add(shoot, this);
+  target.enableBody = true;
+
+  target.body.bounce.x = true;
+  target.body.bounce.y = true;
+
+  target.body.velocity.x = 600 * Math.random();
+  target.body.velocity.y = 600 * Math.random();
+  target.body.collideWorldBounds = true;
+
+};
+
 const preload = () => {
   game.load.image('target', 'img/target.png');
 }
 
 const create = () => {
-  const target = game.add.sprite(100, 120, 'target');
+  game.stage.backgroundColor = '#3498DB';
+  game.physics.startSystem(Phaser.Physics.Arcade);
+  game.renderer.renderSession.roundPixels = true;
+  //target = game.add.sprite(100, 120, 'target');
+  var target = game.add.sprite(100, 120, 'target');
   target.anchor.set(0.5);
   target.scale.set(0.2);
+  game.physics.arcade.enable(target);
   target.inputEnabled = true;
   target.events.onInputDown.add(shoot, this);
+  target.enableBody = true;
+
+  target.body.bounce.x = true;
+  target.body.bounce.y = true;
+
+  target.body.velocity.x = 600 * Math.random();
+  target.body.velocity.y = 600 * Math.random();
+  target.body.collideWorldBounds = true;
 
   this.bmd = game.make.bitmapData(800, 200)
   this.bmd.context.font = '64px Arial'
@@ -47,7 +79,7 @@ const render = () => {
 }
 
 const shoot = () => {
-  socketEvent('shoot', {player: 'me'});
+  socketEvent('shoot', {player: USERNAME});
 };
 
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
