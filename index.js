95e5c90d56ef5dba590a8827ed89c95d894e8c4e
Yasu Flores <carlosyasu91@gmail.com>
2016-11-11 22:27:49 -0800

    New events

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/dist'))
 
-
+const users = []
 
 var server = app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
@@ -15,10 +15,18 @@ app.io = socket_io(server);
 
 // socket io events
 app.io.on('connection', function(socket) {
-  //console.log('User connected');
 
   socket.on('shoot', function(data) {
     console.info(data);
     socket.emit('shoot', data);
   });
+
+  socket.on('saveName', function(name) {
+    users.push({ name: name })
+    socket.emit('namesUpdated', users)
+  })
+
+  socket.on('getNamesList', function(){
+  })
+
 });
