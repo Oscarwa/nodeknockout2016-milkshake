8f6465b48e3071f52d9c9b5e6293948aff247797
Yasu Flores <carlosyasu91@gmail.com>
2016-11-11 23:24:40 -0800

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,18 @@
 import hello from 'x'
+<<<<<<< a1dbb8c5d2ca567e4642f375779331827ac531a2
 var socket = io();
+=======
+import renderNameInfo from 'UI'
+
+const constants = {
+  ENTER_NAME_MSG: 'Please enter your name'
+}
+
+// socket.io events and handles
+const socket = io();
+var target;
+let username = '';
+
 const socketEvent = (eventName, data) => {
   socket.emit(eventName, data);
 }
@@ -11,8 +24,94 @@ socket.on('namesUpdated', (names) => {
   console.log('Users List', names)
   game.showNames(names)
 })
-const constants = {
-  ENTER_NAME_MSG: 'Please enter your name'
+
+//Helper functions
+const keyPress = (char) => {
+  username+=char
+  this.bmd.cls()
+  this.bmd.context.fillText(username, 64, 128)
+}
+
+const deleteCharFromName = () => {
+  username = username.slice(0, username.length-1)
+  this.bmd.cls()
+  this.bmd.context.fillText(username, 64, 128)
+}
+
+const saveName = () => {
+  this.name = username
+  alert(`Saved your name as:${this.name}`)
+  socketEvent('saveName', this.name)
+  this.bmd.cls()
+  this.nameMsg.cls()
+}
+
+const showNames = (names) => {
+
+}
+
+const shoot = () => {
+  socketEvent('shoot', {player: 'me'});
+};
+
+// Phaser io events and handles
+function createTarget() {
+  var target = game.add.sprite(100, 120, 'target');
+  target.anchor.set(0.5);
+  target.scale.set(0.2);
+  game.physics.arcade.enable(target);
+  target.inputEnabled = true;
+  target.events.onInputDown.add(shoot, this);
+  target.enableBody = true;
+
+  target.body.bounce.x = true;
+  target.body.bounce.y = true;
+
+  target.body.velocity.x = 600 * Math.random();
+  target.body.velocity.y = 600 * Math.random();
+  target.body.collideWorldBounds = true;
+
+};
+
+const preload = () => {
+  game.load.image('target', 'img/target.png');
+}
+
+const create = () => {
+  this.game = game
+  game.stage.backgroundColor = '#3498DB';
+  game.physics.startSystem(Phaser.Physics.Arcade);
+  game.renderer.renderSession.roundPixels = true;
+
+  const backspace = game.input.keyboard.addKey(Phaser.KeyCode.BACKSPACE)
+  backspace.onDown.add(deleteCharFromName, this)
+
+  const enter = game.input.keyboard.addKey(Phaser.KeyCode.ENTER)
+  enter.onDown.add(saveName, this)
+
+  var target = game.add.sprite(100, 120, 'target');
+  target.anchor.set(0.5);
+  target.scale.set(0.2);
+  game.physics.arcade.enable(target);
+  target.inputEnabled = true;
+  target.events.onInputDown.add(shoot, this);
+  target.enableBody = true;
+
+  target.body.bounce.x = true;
+  target.body.bounce.y = true;
+
+  target.body.velocity.x = 600 * Math.random();
+  target.body.velocity.y = 600 * Math.random();
+  target.body.collideWorldBounds = true;
+
+  renderNameInfo.bind(this)
+
+  game.input.keyboard.addCallbacks(this, null, null, keyPress)
+}
+
+const update = () => {
+
+>>>>>>> changes
 }
 
 var mainState = {
